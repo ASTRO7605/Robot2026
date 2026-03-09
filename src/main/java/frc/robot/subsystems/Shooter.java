b7810c3de6f6ed24c5c64272a6fad14180d3d7ae
Rick Sebastien Julien <jsebastien2003@gmail.com>
2026-03-09 12:16:11 -0400

    shooter.java

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    
+    public Shooter() {
+        super();
+    }
+
+     @Override
+     public void periodic() {
+         super.periodic();
+     }
+    
+}

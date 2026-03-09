72e6b7a35102c8e25775f7baa685d8a5a24f845d
Rick Sebastien Julien <jsebastien2003@gmail.com>
2026-03-09 12:33:14 -0400

    Code d'Harrison

--- src/main/java/frc/robot/subsystems/ShooterBase.java
+++ src/main/java/frc/robot/subsystems/ShooterBase.java
@@ -1,5 +1,5 @@
 package frc.robot.subsystems;
-
+ 
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
@@ -8,23 +8,42 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterBaseConstants;
-import frc.robot.Constants;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
+ 
 public class ShooterBase extends SubsystemBase {
     // initialisation du moteur et de l'encodeur
-    private SparkMax shootMotor  = new SparkMax(ShooterBaseConstants.shooterBaseMotorId, MotorType.kBrushless);
-    private RelativeEncoder shootEncoder = shootMotor.getEncoder();
+    private SparkMax ShooterBaseMotor  = new SparkMax(ShooterBaseConstants.shooterBaseMotorId, MotorType.kBrushless);
+    private RelativeEncoder ShooterBaseEncoder = ShooterBaseMotor.getEncoder();
     private SparkMaxConfig currentConfig;
-    
-    // constructeur du sous-système
+   
     public ShooterBase() {
-
+     currentConfig = new SparkMaxConfig();
+     currentConfig.idleMode(SparkMaxConfig.IdleMode.kBrake);
+     currentConfig.inverted(false);
+ 
+     // limitation du courant et de la tension pour protéger le moteur et la batterie
+        currentConfig.voltageCompensation(ShooterBaseConstants.kVoltageCompensation);
+        currentConfig.smartCurrentLimit(ShooterBaseConstants.kCurrentLimit);
+       
+        ShooterBaseMotor.configure(currentConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
-
+ 
+    // fait tourner les roues de la shooter base pour faire entrer et apporter les balles
+    public void ShooterBaseWheelsIn(){
+        ShooterBaseMotor.set(ShooterBaseConstants.kInSpeed);
+    }
+ 
+     // arrête les roues du shooter
+    public void ShooterBaseWheelOff(){
+        ShooterBaseMotor.stopMotor();
+    }
+ 
+    public boolean isShooterBaseStopped() {
+        return Math.abs(ShooterBaseEncoder.getVelocity()) < ShooterBaseConstants.kThresholdMotorStopped;
+    }
+ 
     @Override
     public void periodic() {
-        
+       
     }
-
+ 
 }
